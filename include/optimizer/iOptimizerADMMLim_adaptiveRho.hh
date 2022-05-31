0ac6ec9afacd7f87951694ce47a7b20ef758089d
liutie <liutie667@gmail.com>
2022-05-31 17:00:56 +0200

    set optimizer to their own name

--- include/optimizer/iOptimizerADMMLim_adaptiveRho.hh
+++ include/optimizer/iOptimizerADMMLim_adaptiveRho.hh
@@ -270,7 +270,7 @@ class iOptimizerADMMLim_adaptiveRho : public vOptimizer
 
 // Class for automatic insertion (set here the visible optimizer's name as the first parameter,
 // put the class name as the second parameter and do NOT add semi-colon at the end of the line)
-CLASS_OPTIMIZER(ADMMLim,iOptimizerADMMLim_adaptiveRho)
+CLASS_OPTIMIZER(ADMMLim_adaptiveRho,iOptimizerADMMLim_adaptiveRho)
 
 #endif
 
